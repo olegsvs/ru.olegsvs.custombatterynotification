f62d5d846c151f1d9c165950b38c030b7263dfc2
olegsvs <oleg.texet@gmail.com>
2017-05-11 18:01:30 +0300

    Create class BatteryManager

---
+++ app/src/androidTest/java/ru/olegsvs/custombatterynotification/BatteryManager.java
@@ -0,0 +1,17 @@
+package ru.olegsvs.custombatterynotification;
+
+/**
+ * Created by user on 11.05.2017.
+ */
+
+public class BatteryManager {
+    private static final String SYS_BATTERY_CAPACITY = "/sys/class/power_supply/battery/capacity";
+    private static final String SYS_BATTERY_STATUS = "/sys/class/power_supply/battery/status";
+    private static final String SYS_BATTERY_CAPACITY_JSR = "/sys/class/power_supply/batteryjsr/capacity";
+    private static final String SYS_BATTERY_STATUS_JSR = "/sys/class/power_supply/batteryjsr/status";
+    public boolean IS_JSR_SUPPORT = false;
+
+    BatteryManager() {
+        
+    }
+}
